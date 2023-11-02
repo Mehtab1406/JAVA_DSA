49e4e07ff2554d6800fa15dcb544ad092cd8fc8e
Mehtab Bibi <107465446+Mehtab1406@users.noreply.github.com>
2023-11-02 23:58:41 +0530

    Add files via upload

---
+++ AdvancedPattern.java
@@ -0,0 +1,45 @@
+public class AdvancedPattern {
+    // Q1: PRINT HOLLOW RECTANGLE PATTERN
+
+    public static void hollow_rectangle(int totRows, int totCols) {
+        // outer loop
+        for (int i = 1; i <= totRows; i++) {
+            // inner - columns
+            for (int j = 1; j <= totCols; j++) {
+                // cell - (i,j)
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    // boundry cells
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    // Q2: INVERTED & ROTATED HALF-PYRAMID
+    public static void inverted_rotated_half_pyramid(int n) {
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // hollow_rectangle(4, 5);
+        inverted_rotated_half_pyramid(20);
+
+    }
+}
